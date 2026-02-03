01b05bfcb5c3cac9f3ae08c3d942ee4d715965d9
LeDinhoo <le.dinho.dev@gmail.com>
2026-02-03 17:20:24 +0100

    feat: add sorting functionality to data table and disable sorting for specific columns

--- src/routes/ventes/data-table.svelte
+++ src/routes/ventes/data-table.svelte
@@ -1,10 +1,11 @@
 <script lang="ts" generics="TData, TValue">
-  import { type ColumnDef, getCoreRowModel } from "@tanstack/table-core";
+  import { type ColumnDef, getCoreRowModel, getSortedRowModel, type SortingState } from "@tanstack/table-core";
   import {
     createSvelteTable,
     FlexRender,
   } from "$lib/components/ui/data-table/index.js";
   import * as Table from "$lib/components/ui/table/index.js";
+  import { ArrowUpDown, ArrowUp, ArrowDown } from "@lucide/svelte";
 
   type DataTableProps<TData, TValue> = {
     columns: ColumnDef<TData, TValue>[];
@@ -13,12 +14,27 @@
 
   let { data, columns }: DataTableProps<TData, TValue> = $props();
 
+  let sorting = $state<SortingState>([]);
+
   const table = createSvelteTable({
     get data() {
       return data;
     },
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: (updater) => {
+      if (typeof updater === "function") {
+        sorting = updater(sorting);
+      } else {
+        sorting = updater;
+      }
+    },
+    state: {
+      get sorting() {
+        return sorting;
+      },
+    },
   });
 </script>
 
@@ -30,10 +46,30 @@
           {#each headerGroup.headers as header (header.id)}
             <Table.Head colspan={header.colSpan}>
               {#if !header.isPlaceholder}
-                <FlexRender
-                  content={header.column.columnDef.header}
-                  context={header.getContext()}
-                />
+                {#if header.column.getCanSort()}
+                  <button
+                    type="button"
+                    class="flex items-center gap-1 hover:text-foreground transition-colors cursor-pointer select-none"
+                    onclick={() => header.column.toggleSorting()}
+                  >
+                    <FlexRender
+                      content={header.column.columnDef.header}
+                      context={header.getContext()}
+                    />
+                    {#if header.column.getIsSorted() === "asc"}
+                      <ArrowUp class="size-4" />
+                    {:else if header.column.getIsSorted() === "desc"}
+                      <ArrowDown class="size-4" />
+                    {:else}
+                      <ArrowUpDown class="size-4 opacity-50" />
+                    {/if}
+                  </button>
+                {:else}
+                  <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                  />
+                {/if}
               {/if}
             </Table.Head>
           {/each}
